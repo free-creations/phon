0bc16080f9908c40ef7ac6fb2cd60347923ca11f
harald <harald@ObelixWin>
2014-03-20 18:29:00 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ phonDb/CreateDb/LibreOffice/PersonAllocatonQuery.sql
@@ -0,0 +1,77 @@
+/*
+Query to produce the document "Anschreiben.doc".
+
+*/
+SELECT 
+  "ALLOC"."ALLOCATIONID",
+  "ALLOC"."PERSON" AS "PersonId",
+  "ALLOC"."JOB" AS "JobId",
+  "CONTEST"."CONTESTID",
+  "CONTEST"."DESCRIPTION" AS "ContestDescription",
+  "CONTEST"."NAME"  AS "ContestName",
+  "PERSON"."SURNAME" AS "PersonSurname", 
+  "PERSON"."GIVENNAME" AS "PersonGivenName", 
+  "Responsible"."SURNAME" AS "RespSurname", 
+  "Responsible"."GIVENNAME" AS "RespGivenname", 
+  "Teacher"."SURNAME" AS "TeacherSurname", 
+  "Teacher"."GIVENNAME" AS "TeacherGivenname", 
+  "PERSON"."TELEPHONE",   
+  "PERSON"."ZIPCODE",   
+  "PERSON"."CITY",   
+  "PERSON"."STREET", 
+  "PERSON"."MOBILE", 
+  "PERSON"."EMAIL", 
+  "TIMESLOT"."TIMESLOTID",  
+  "TIMESLOT"."STARTTIME",  
+  "TIMESLOT"."ENDTIME", 
+  "TIMESLOT"."DAYOFWEEK", 
+  "TIMESLOT"."DAYIDX",
+  "TIMESLOT"."DATUM", 
+  "TIMESLOT"."TIMEOFDAYPRINT", 
+  "TIMESLOT"."TIMEOFDAYIDX",
+  "TIMESLOT"."LABEL" AS "TimeLabel",
+  "LOCATION"."NAME" AS "LocationName",
+  "LOCATION"."BUILDING",
+  "LOCATION"."ROOM",
+  "LOCATION"."STREET",
+  "JOBTYPE"."JOBTYPEID",
+  "JOBTYPE"."NAME" AS "JobTypeName",
+  "AllocTeacher"."PERSON" AS "TeacherId"
+/**/
+FROM 
+  "APP"."ALLOCATION" AS "ALLOC"
+
+   LEFT JOIN "APP"."PERSON" AS "PERSON"
+   ON "ALLOC"."PERSON" = "PERSON"."PERSONID" 
+
+   LEFT JOIN   "APP"."EVENT" AS "EVENT"
+   ON "ALLOC"."EVENT" = "EVENT"."EVENTID"
+
+   LEFT JOIN "APP"."TIMESLOT" AS "TIMESLOT"
+   ON "EVENT"."TIMESLOT" = "TIMESLOT"."TIMESLOTID" 
+
+   LEFT JOIN  "APP"."CONTEST" AS "CONTEST"
+   ON  "EVENT"."CONTEST" = "CONTEST"."CONTESTID"
+
+   LEFT JOIN  "APP"."LOCATION" AS "LOCATION"
+   ON  "EVENT"."LOCATION" = "LOCATION"."LOCATIONID"
+
+   LEFT JOIN "APP"."PERSON" AS "Responsible"
+   ON "CONTEST"."PERSON" = "Responsible"."PERSONID" 
+
+   LEFT JOIN "APP"."JOB" AS "JOB"
+   ON "ALLOC"."JOB" = "JOB"."JOBID"
+
+   LEFT JOIN "APP"."JOBTYPE" AS "JOBTYPE"
+   ON "JOB"."JOBTYPE" = "JOBTYPE"."JOBTYPEID"
+
+   LEFT JOIN "APP"."ALLOCATION" AS "AllocTeacher"
+   ON "AllocTeacher"."EVENT" = "EVENT"."EVENTID"
+
+   LEFT JOIN "APP"."PERSON" AS "Teacher"
+   ON "AllocTeacher"."PERSON" = "Teacher"."PERSONID" 
+WHERE
+  "AllocTeacher"."JOB" = 'LEHRER'
+
+
+
